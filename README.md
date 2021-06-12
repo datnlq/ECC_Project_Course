c1ee9d5bbaa262b1ef5c8382c7c600a7f788f30d
Datnlq <datnlquiter@gmail.com>
2021-06-12 13:23:47 +0700

    ECC_App

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 Bước 1: Install python 3.9 cho Visual Studio Code [video hướng dẫn](https://www.youtube.com/watch?v=dNFgRUD2w68&t=94s)
 
 Bước 2: Install PyQt5 (thư viện) và PyQt5-tools (giao diện kéo thả) [video hướng dẫn](https://www.youtube.com/watch?v=aEUR-ngeXNA)
+
+
+## Set up môi trường chạy source code 
+# ---------------------------------------
+# Installation
+
+Set up môi trường cho ECC
+Source: https://github.com/lc6chang/ecc-pycrypto
+
+```
+git clone https://github.com/lc6chang/ecc-pycrypto
+cd ecc-pycrypto
+pip3 install .
+
+```
+
+Set up môi trường cho Crypto
+Source:https://pypi.org/project/pycrypto/
+```
+pip install pycrypto
+```
---
+++ Source_Demo/Demo/InterfaceVer2/demoSQL.py
@@ -0,0 +1,191 @@
+import sqlite3
+from sqlite3 import Error
+import hashlib as h
+
+
+def connect(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+       # print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create(conn, sql):
+
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+    except Error as e:
+        print(e)
+
+def insert_student(conn, student):
+
+    sql = ''' INSERT INTO STUDENT(StudentID,Fullname,Sex,Year,Faculty,Phone,Email,PublicKey)
+              VALUES(?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, student) #student = ('19521336','Nguyen Le Quoc Dat','Nam','K14','MMT&TT','0976828232','19521336@gm.uit.edu.vn','ascascascasc' ) 
+    conn.commit()
+    return cur.lastrowid
+
+def insert_teacher(conn, teacher):
+
+    sql = ''' INSERT INTO TEACHER(TeacherID,Fullname,Sex,Faculty,Phone,Email,PrivateKey)
+              VALUES(?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, teacher) #teacher = ('1952',' Quoc Dat','Nam','MMT&TT','0976828232','19521336@gm.uit.edu.vn','gessing' ) 
+    conn.commit()
+    return cur.lastrowid
+
+def insert_result(conn, result):
+
+    sql = ''' INSERT INTO RESULT(StudentID,ModuleID,ProcessPoint,MidPoint,LabPoint,EndPoint,AveragePoint,Note)
+              VALUES(?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, result ) #result = ('19521336','NT216.ATTT.L21','10','10','10','10','gessing' ) 
+    conn.commit()
+    return cur.lastrowid
+
+def insert_module(conn, module):
+    sql = """INSERT INTO MODULE(ModuleID,NameSubject,TeacherID,Semester,SchoolYear) 
+                        VALUES(?,?,?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, module) #module = ('NT216.ATTT.L21','Mat ma hoc','1952','1','2020-2021' ) 
+    conn.commit()
+    return cur.lastrowid
+
+def update(conn, student):
+
+    sql = ''' UPDATE student
+              SET Name = ? ,
+                  Math = ? ,
+                  English = ?
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, student)
+    conn.commit()
+
+def delete_by_id(conn, id):
+    sql = 'DELETE FROM student WHERE id=?'
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    conn.commit()
+
+def selectStudent(conn, username):
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM STUDENT WHERE StudentID=?", (username,))
+
+    row = cur.fetchone()
+    return row
+
+def selectTeacher(conn, username):
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM TEACHER WHERE TeacherID=?", (username,))
+
+    row = cur.fetchone()
+    return row
+
+def check(passCheck, passInput):
+    hash_passCheck = h.sha1(bytes(passCheck, 'utf-8')).hexdigest()
+    hash_passInput = h.sha1(bytes(passInput, 'utf-8')).hexdigest()
+    if(hash_passCheck == hash_passInput):
+        print("Chuc mung!")
+    else:
+        print("Password sai roy!")
+
+
+def checkPass(conn, username, passInput):
+    infor = selectStudent(conn, username)
+
+    if(infor == None):
+        infor = selectTeacher(conn, username)
+        
+        if (infor == None):
+            print("Username sai roy!")
+        else:
+            check(infor[7], passInput)
+            print("Ban la giao vien!")
+    else:
+        check(infor[7], passInput)
+        print("Ban la hoc sinh!")
+
+if __name__ == '__main__':
+
+    conn = connect(r"ECCapp.db")
+    username = "19521336"
+    password = "simpboiz"
+    checkPass(conn,username,password)
+
+    cur.execute("""SELECT * FROM STUDENT""")
+    print(cur.fetchone())
+
+
+
+    '''cur = conn.cursor()
+    cur.execute("""SELECT * FROM STUDENT""")
+    print(cur.fetchone()[7])'''
+
+
+
+    '''sql= """CREATE TABLE IF NOT EXISTS student (
+    id integer PRIMARY KEY,
+    Name text NOT NULL,
+    Math float,
+    English float
+    );
+    """
+    create(conn,sql)
+    
+    student = ('19521336','Nguyen Le Quoc Dat','Nam','K14','MMT&TT','0976828232','19521336@gm.uit.edu.vn','ascascascasc' ) 
+    teacher = ('19521336','Nguyen Le Quoc Dat','Nam','MMT&TT','0976828232','19521336@gm.uit.edu.vn','gessingcxxc' ) 
+    module = ('NT216.ATTT.L21','Mat ma hoc','1952','Hoc ky 1','2020-2021' )
+    result = ('19521336','NT216.ATTT.L21','10','10','10','10','gessingxs' ) 
+
+
+    try:
+
+        insert_student(conn,student)
+    except Error as e:
+        print(e)
+
+    try:
+        insert_teacher(conn,teacher)
+    except Error as e:
+        print(e)
+
+        CREATE TABLE "RESULT" (
+	"StudentID"	TEXT NOT NULL,
+	"ModuleID"	TEXT NOT NULL,
+	"ProcessPoint"	REAL,
+	"MidPoint"	REAL,
+	"LabPoint"	REAL,
+	"EndPoint"	REAL,
+	"AveragePoint"	REAL,
+	"Note"	TEXT,
+	FOREIGN KEY("ModuleID") REFERENCES "MODULE"("ModuleID"),
+	FOREIGN KEY("StudentID") REFERENCES "STUDENT"("StudentID"),
+	PRIMARY KEY("StudentID","ModuleID"),
+	CHECK((AveragePoint<3 and Note='Khong dat') or (AveragePoint>=3 and Note='Dat'))
+);
+
+--SELECT ModuleID FROM MODULE WHERE TeacherID = "anv" and Semester = "Hoc ky 2" and SchoolYear = "2020-2021";
+--SELECT st.StudentID, st.Fullname, rs.ProcessPoint, rs.MidPoint, rs.LabPoint,EndPoint,rs.AveragePoint,rs.Note
+--FROM STUDENT st, RESULT rs, MODULE md
+--WHERE st.StudentID = rs.StudentID and rs.ModuleID = md.ModuleID and md.ModuleID = "NT216.L21.ANTT";
+--SELECT md.ModuleID, md.NameSubject, md.Cre
+CREATE TABLE "RESULT" (
+
+    try:
+        insert_module(conn,module)
+    except Error as e:
+        print(e)
+    try:
+        insert_result(conn,result)
+    except Error as e:
+        print(e)'''
+    
+    
